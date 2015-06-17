d9ae2b904928cc89a1ae0f6d0d9e9ea80143a28d
anneal <anneal@users.noreply.github.com>
2015-06-17 12:21:26 -0400

    Code to parse the datafile

---
+++ csvWithMeta.py
@@ -0,0 +1,27 @@
+"""
+csvWithMeta.py
+"""
+
+from astropy.io import ascii
+
+def readCSV(file):
+    my_data = ascii.read(file, comment = r'\s*@')
+    keys = []
+    values = []
+    for element in my_data.meta['comments']:
+        key, value = element.split()
+        keys.append(key)
+        values.append(float(value))
+        
+    params = dict(zip(keys, values))
+
+    return params, my_data
+
+if __name__ == '__main__':
+    filename = 'test.csv'
+
+    p, d = readCSV(filename)
+    print(p)
+    print(d)
+
+    
